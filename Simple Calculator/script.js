64ef0b54157c3a696656811fd940328cc376e463
Minahj05 <efti3101@gmail.com>
2026-09-21 19:08:40 -0700

    Add files via upload

---
+++ Simple Calculator/script.js
@@ -0,0 +1,50 @@
+
+function add() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let result = num1 + num2;
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+
+function subtract() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let result = num1 - num2;
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+
+function multiply() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    let result = num1 * num2;
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+
+function divide() {
+
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+
+    if (num2 === 0) {
+        document.getElementById("result").innerText =
+            "Cannot divide by zero!";
+        return;
+    }
+
+    let result = num1 / num2;
+
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
